9590209a5ba13f245be55a944052896a42e0a5b6
PavelAlexee <pavelps41980@gmail.com>
2025-10-06 20:10:14 +0300

    completed the task for the lesson 'Testing controllers'

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    it 'assigns a new Answer to @answer' do
+      get :new, params: { question_id: question }
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      get :new, params: { question_id: question }
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer in the database' do
+        expect {
+          post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to question show view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        expect(response).to redirect_to(question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect {
+          post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        }.not_to change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
